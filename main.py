63bdbee74d099c510564ee0a1a6cb763d8647942
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-09-06 11:55:29 +0200

    error is min > max

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import random
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -8,6 +8,10 @@ app = FastAPI()
 async def rng(min: int, max: int):
     min_n = min
     max_n = max 
+
+    if min_n > max_n:
+        raise HTTPException(status_code=400, detail=f'{min=} is larger than {max=}')
+
     number = random.randint(min_n, max_n)
     return {'lowerLimit': min_n, 'higherLimit': max_n, 'number': number}
 
